93c9cf4b54f6b84a95713dd0184428f53c3521cc
Edy Alves <edy4alves@gmail.com>
2021-05-27 08:39:06 -0300

    Fist class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello");
+		Position pos = new Position(3, 4);
 
+		System.out.println(pos);
 	}
 
 }
