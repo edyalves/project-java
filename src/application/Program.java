35f2e15c0e3cc574ab0e04633736c32309facf65
Edy Alves <edy4alves@gmail.com>
2021-05-27 09:44:55 -0300

    Implementing board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,15 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Position pos = new Position(3, 4);
+		ChessMatch chessMatch = new ChessMatch();
 
-		System.out.println(pos);
+		//PRINT FUNCTION
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
